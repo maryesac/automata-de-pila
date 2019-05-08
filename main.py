c91387078e9ec9cc381da14d14c94984d6c483fc
Marlon Acuña <maryesacu@gmail.com>
2019-05-08 14:47:34 -0500

    otra vez

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 from frontend.validador import Validador
 from frontend.ventana import Ventana
 
 ventana = Ventana()
-=======
-from factory import Factory
-if __name__ == '__main__':
-	creador = Factory()
-	print("hola1")
-	automata = creador.get_automata("abcba")
-
-	print(automata) 
-	#Factory.transiciones(automata)
->>>>>>> 29d8c587802264f3b337f2bbdcd4d2ae6b8738ea
